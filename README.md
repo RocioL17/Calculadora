eba5eba331941e526b36c2761a191510879349d2
RocioL17 <rociolearreta@gmail.com>
2025-04-02 23:56:08 -0300

    feat/diagrams + instructions added

--- README.md
+++ README.md
@@ -5,8 +5,21 @@ Esta calculadora nos permite realizar las operaciones básicas de la Estadístic
 Para utilizarla debemos descargas las siguientes librerías:
 -> pip install scipy
 -> pip install statistics
+-> pip install seaborn
 
 Luego para poner a correr el código, a esta altura de carpeta:
 -> python menu.py
 
 Debemos ingresar un listado de números divididos por espacios.
+En base a esto, podremos realizar una serie de medidas, tanto de localización como variabilidad.
+
+Nuevas funcionalidades:
+- Diagramas:
+  - Histograma
+  - Caja y Bigote
+  - Diagrama de dispersión
+
+- Ingresar una lista nueva sin salir del programa
+
+Cualquier duda consultar :)
+Su servidora, Rocio Learreta
--- menu.py
+++ menu.py
@@ -4,6 +4,8 @@ import mediana
 import media_recortada
 import varianza
 import desviacion
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def mostrar_menu():
     print("\nMenú de Cálculo Estadístico")
@@ -14,20 +16,73 @@ def mostrar_menu():
     print("5. Calcular varianza")
     print("6. Calcular desviación estándar")
     print("7. Mostrar la lista actual")
-    print("8. Salir")
+    print("8. Cambiar la lista de números")
+    print("9. Diagramas")
+    print("10. Salir")
+
+def mostrar_histograma(lista):
+    sns.histplot(lista, bins=10, color='#722F37', kde=True)
+    plt.title("Histograma")
+    plt.show()
+
+def mostrar_boxplot(lista):
+    sns.boxplot(x=lista, color='#034F84')
+    plt.title("Caja y Bigote")
+    plt.show()
+
+def mostrar_scatterplot(lista):
+    print("\nOpciones para scatterplot:")
+    print("1. Usar la misma lista")
+    print("2. Ingresar una nueva lista para comparar")
+    opcion = input("Selecciona una opción: ")
+    
+    if opcion == "1":
+        plt.scatter(range(len(lista)), lista, color='#722F37', label="Lista Original")
+        plt.plot(range(len(lista)), lista, color='#034F84', linestyle='dashed')
+    elif opcion == "2":
+        nueva_lista = list(map(float, input("Ingresa una nueva lista de números separados por espacios: ").split()))
+        plt.scatter(range(len(lista)), lista, color='#722F37', label="Lista 1")
+        plt.scatter(range(len(nueva_lista)), nueva_lista, color='#034F84', label="Lista 2")
+    else:
+        print("Opción no válida.")
+        return
+    
+    plt.title("Scatterplot")
+    plt.legend()
+    plt.show()
+
+def mostrar_diagramas(lista):
+    print("\nElige un diagrama:")
+    print("1. Histograma")
+    print("2. Caja y Bigote")
+    print("3. Diagrama de Dispersión")
+    opcion = input("Selecciona una opción: ")
+    
+    if opcion == "1":
+        mostrar_histograma(lista)
+    elif opcion == "2":
+        mostrar_boxplot(lista)
+    elif opcion == "3":
+        mostrar_scatterplot(lista)
+    else:
+        print("Opción no válida.")
 
 def main():
     global lista_numeros
-    numeros = input("Ingresa una lista de números separados por espacios: ")
-    lista_numeros = list(map(float, numeros.split()))
+    
+    def ingresar_lista():
+        numeros = input("Ingresa una lista de números separados por espacios: ")
+        return list(map(float, numeros.split()))
+    
+    lista_numeros = ingresar_lista()
 
     while True:
         mostrar_menu()
-        print(lista_numeros)
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
             lista_numeros = ordenamiento.ordenar_numeros(lista_numeros)
+            print("Lista ordenada:", lista_numeros)
         elif opcion == "2":
             print("La media es:", media.calcular_media(lista_numeros))
         elif opcion == "3":
@@ -42,6 +97,10 @@ def main():
         elif opcion == "7":
             print("Lista actual:", lista_numeros)
         elif opcion == "8":
+            lista_numeros = ingresar_lista()
+        elif opcion == "9":
+            mostrar_diagramas(lista_numeros)
+        elif opcion == "10":
             print("Saliendo del programa...")
             break
         else:
