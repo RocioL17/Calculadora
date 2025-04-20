8fdaec03a3565e4d93fe239899580b214495ba6d
RocioL17 <rociolearreta@gmail.com>
2025-04-20 16:15:47 -0300

    graficos de frecuencia relativa y absoluta

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ import varianza
 import desviacion
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 def mostrar_menu():
     print("\nMenú de Cálculo Estadístico")
@@ -18,6 +19,7 @@ def mostrar_menu():
     print("7. Mostrar la lista actual")
     print("8. Cambiar la lista de números")
     print("9. Diagramas")
+    print("11. cantidad")
     print("10. Salir")
 
 def mostrar_histograma(lista):
@@ -51,6 +53,35 @@ def mostrar_scatterplot(lista):
     plt.legend()
     plt.show()
 
+data = {
+    "Intervalo": ["100-155", "156-211", "212-267", "268-323", "324-379", "380-435", "436-490"],
+    "Punto_medio": [127.5, 183.5, 239.5, 295.5, 351.5, 407.5, 463],
+    "Frecuencia_absoluta": [5, 7, 6, 4, 3, 3, 2],
+    "Frecuencia_relativa": [0.16, 0.23, 0.2, 0.13, 0.1, 0.1, 0.067]
+}
+
+def histograma_parcial():
+    df = pd.DataFrame(data)
+    # Gráfico de Frecuencia Absoluta
+    plt.figure(figsize=(10, 5))
+    sns.barplot(x=df["Punto_medio"], y=df["Frecuencia_absoluta"], color="blue")
+    plt.xlabel("Punto Medio de Intervalo")
+    plt.ylabel("Frecuencia Absoluta")
+    plt.title("Histograma de Frecuencia Absoluta")
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.show()
+
+    # Gráfico de Frecuencia Relativa
+    plt.figure(figsize=(10, 5))
+    sns.barplot(x=df["Punto_medio"], y=df["Frecuencia_relativa"], color="green")
+    plt.xlabel("Punto Medio de Intervalo")
+    plt.ylabel("Frecuencia Relativa")
+    plt.title("Histograma de Frecuencia Relativa")
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.show()
+
 def mostrar_diagramas(lista):
     print("\nElige un diagrama:")
     print("1. Histograma")
@@ -64,6 +95,8 @@ def mostrar_diagramas(lista):
         mostrar_boxplot(lista)
     elif opcion == "3":
         mostrar_scatterplot(lista)
+    elif opcion == "4":
+        histograma_parcial()
     else:
         print("Opción no válida.")
 
@@ -100,6 +133,8 @@ def main():
             lista_numeros = ingresar_lista()
         elif opcion == "9":
             mostrar_diagramas(lista_numeros)
+        elif opcion == "11": 
+            print("lenght: ", len(lista_numeros))
         elif opcion == "10":
             print("Saliendo del programa...")
             break
